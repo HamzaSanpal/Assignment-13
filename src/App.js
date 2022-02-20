2c9aa64af430ff2340b744d2d33572050283ad3d
Hamza Sanpal <94125516+HamzaSanpal@users.noreply.github.com>
2022-02-20 11:26:27 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Header from "./components/Nav Bar/navBar";
+import Hamza from "./components/Header/header";
+function App() {
+  return (
+<div >
+     <Hamza/>
+</div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './headerstyle.css';
+export default function header() {
+  return (
+    <div class="container">
+    <div class="row">
+        <div class="col-6" id="heading">
+            <h1>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, error, voluptates sunt dolorem
+                beatae
+                recusandae soluta dignissimos perspiciatis ratione voluptatibus reprehenderit suscipit perferendis
+                velit
+                quibusdam asperiores incidunt! Beatae, soluta praesentium!
+            </p>
+            <p>
+                Continue reading......
+            </p>
+        </div>
+        <div class="col-6"></div>
+    </div>
+</div>
+  )
+}
+
